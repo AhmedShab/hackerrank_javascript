4fc90e30473e9b9c44bc9a7a77c392205e776d49
AhmedShab <ahmed.vuw@gmail.com>
2017-07-11 16:13:26 +1200

    done the challenge

---
+++ funnyString.js
@@ -0,0 +1,34 @@
+function funnyString(s, r){
+    for (let i = 1; i < s.length; i++) {
+        let text = s.charAt(i).charCodeAt(0);
+        let textToCompare = s.charAt(i - 1).charCodeAt(0);
+
+        let reverse = r.charAt(i).charCodeAt(0);
+        let reverseToCompare = r.charAt(i - 1).charCodeAt(0);
+
+        // console.log(`text ${text} textTwo ${textToCompare}`);
+        // console.log(`reverse ${reverse} reverseTwo ${reverseToCompare}`);
+
+        if (Math.abs(text - textToCompare) !== Math.abs(reverse - reverseToCompare)) {
+            return 'Not Funny';
+        }
+
+        else if (i === s.length - 1){
+            return 'Funny';
+        }
+    }
+}
+
+function main() {
+    var q = 1;
+    for(var a0 = 0; a0 < q; a0++){
+        var s = 'acxz';
+        var r = s.split('').reverse().join('');
+
+        var result = funnyString(s, r);
+        process.stdout.write('' + result + '\n');
+    }
+
+}
+
+main();
